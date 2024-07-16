3acb4a46172b80892b4f2e3739723f1eb3dcbdc2
Velber <goncalvesvelber@gmail.com>
2024-07-15 21:50:02 -0300

    *

---
+++ catalogo.js
@@ -0,0 +1,6 @@
+let imgJogadores = ["https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.si.com%2Fnba%2Frockets%2Fnews%2Frockets-ime-udoka-praises-jalen-green-after-first-career-triple-double&psig=AOvVaw3fd6fEr5wc_8flYNr_beJF&ust=1721167152935000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCPi2t7GFqocDFQAAAAAdAAAAABAE",]
+let nomeJogadores = ["Jalen Green",]
+let linkJogadores = ["https://www.youtube.com/watch?v=2-m8wqOVr-g",]
+for(let i=0;i<imgJogadores.length;i++){
+    document.write("<div class='itens'>", "<a href=",linkJogadores[i], "target='_blank'>",<img class='$ scr=$,{imgJogadores}></img>)
+}
